af2c2485ffdb21e688478c9920f4c93c65f11458
iqbal125 <iqbal125@yahoo.com>
2020-10-06 18:26:28 -0700

    fix router issue

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", function (req, res, next) {
+  res.render("index", {title: "Express"})
+})
 
-module.exports = router;
+router.get("/health", (req, res) => {
+  res.send("Server Working")
+})
+
+module.exports = router
